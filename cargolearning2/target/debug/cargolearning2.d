7ae72aefdf289f967f8badcfcc2b5f46416bd63e
dev <dev@v-rust-Ubuntu23.10mantic6.5.0-14-generic>
2024-08-28 18:32:51 +0000

    new files sh

--- cargolearning2/target/debug/cargolearning2.d
+++
@@ -1,1 +0,0 @@
-/home/dev/wf/rustlearning/cargolearning2/target/debug/cargolearning2: /home/dev/wf/rustlearning/cargolearning2/src/main.rs
--- cargolearning2/target/debug/deps/cargolearning2-08bc007bb309d4db.d
+++
@@ -1,5 +0,0 @@
-/home/dev/wf/rustlearning/cargolearning2/target/debug/deps/cargolearning2-08bc007bb309d4db: src/main.rs
-
-/home/dev/wf/rustlearning/cargolearning2/target/debug/deps/cargolearning2-08bc007bb309d4db.d: src/main.rs
-
-src/main.rs:
